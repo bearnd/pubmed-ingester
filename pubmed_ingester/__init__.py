6dff9a94cd6622a27ef41c0f77e49a89b53669c2
Adamos Kyriakou <somada141@gmail.com>
2017-11-12 00:07:52 +1100

    `__init__.py`: Updated with imports for the new modules.

--- pubmed_ingester/__init__.py
+++ pubmed_ingester/__init__.py
@@ -2,6 +2,13 @@
 
 """Top-level package for pubmed-ingester."""
 
+from . import config
+from . import excs
+from . import loggers
+from . import orm_base
+from . import sql
+from . import pubmed_ingester
+
 __author__ = """Adamos Kyriakou"""
 __email__ = 'adam@bearnd.io'
 __version__ = '0.1.0'
